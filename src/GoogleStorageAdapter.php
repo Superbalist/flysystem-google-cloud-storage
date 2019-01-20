e35c6c788304e39995a716de836d8bbd97858ab9
Kendryck <kainxspirits@users.noreply.github.com>
2019-01-20 19:12:57 +0100

    Delete recursively all objects inside a dir before deleting a dir.

--- src/GoogleStorageAdapter.php
+++ src/GoogleStorageAdapter.php
@@ -249,7 +249,32 @@ class GoogleStorageAdapter extends AbstractAdapter
      */
     public function deleteDir($dirname)
     {
-        return $this->delete($this->normaliseDirName($dirname));
+        $dirname = $this->normaliseDirName($dirname);
+        $objects = $this->listContents($dirname, true);
+
+        // We first delete the file, so that we can delete
+        // the empty folder at the end.
+        uasort($objects, function ($a, $b) {
+            return $b['type'] === 'file' ? 1 : -1;
+        });
+
+        // We remove all objects that should not be deleted.
+        $filtered_objects = array_filter($objects, function ($object) use ($dirname) {
+            return strpos($object['path'], $dirname) !== false;
+        }, true);
+
+        // Execute deletion for each object.
+        foreach ($filtered_objects as $object) {
+            $path = $object['path'];
+
+            if ($object['type'] === 'dir') {
+                $path = $this->normaliseDirName($path);
+            }
+
+            $this->delete($path);
+        }
+
+        return true;
     }
 
     /**
